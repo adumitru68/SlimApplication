3abdc3529772f50beb710cfca72ea244a6ecd225
adiDumitru <adrian,dumitru.68@gmail.com>
2017-11-30 00:38:44 +0200

    Trailing middleware reformat

--- src/Middleware/TrailingSlash.php
+++ src/Middleware/TrailingSlash.php
@@ -11,6 +11,7 @@ namespace Qpdb\SlimApplication\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Http\Response;
 
 class TrailingSlash
 {
@@ -25,51 +26,47 @@ class TrailingSlash
 	 *
 	 * @param bool $addSlash
 	 */
-	public function __construct($addSlash = false)
+	public function __construct( $addSlash = false )
 	{
-		$this->addSlash = (bool) $addSlash;
+		$this->addSlash = (bool)$addSlash;
 	}
 
 	/**
 	 * Execute the middleware.
 	 *
 	 * @param ServerRequestInterface $request
-	 * @param ResponseInterface      $response
-	 * @param callable               $next
+	 * @param ResponseInterface|Response $response
+	 * @param callable $next
 	 *
 	 * @return ResponseInterface
 	 */
-	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+	public function __invoke( ServerRequestInterface $request, ResponseInterface $response, callable $next )
 	{
 		$uri = $request->getUri();
 		$path = $uri->getPath();
 
 		//Add/remove slash
-		if (strlen($path) > 1) {
-			if ($this->addSlash) {
-				if (substr($path, -1) !== '/' && !pathinfo($path, PATHINFO_EXTENSION)) {
+		if ( strlen( $path ) > 1 ) {
+			if ( $this->addSlash ) {
+				if ( substr( $path, -1 ) !== '/' && !pathinfo( $path, PATHINFO_EXTENSION ) ) {
 					$path .= '/';
 				}
-			} else {
-				$path = rtrim($path, '/');
-			}
-		}
-//		elseif ($path === '') {
-//			$path = '/';
-//		}
-
-		//var_dump($uri->getPath(), $path);
-
-		//redirect
-		if ($uri->getPath() !== $path) {
-			if($request->getMethod() == 'GET') {
-				return $response->withRedirect((string)$path, 301);
 			}
 			else {
-				return $next($request->withUri($uri), $response);
+				$path = rtrim( $path, '/' );
 			}
 		}
 
-		return $next($request, $response);
+		//redirect
+		if ( $uri->getPath() !== $path ) {
+			if ( $request->getMethod() == 'GET' ) {
+				return $response->withRedirect( (string)$path, 301 );
+			}
+			else {
+				return $next( $request->withUri( $uri ), $response );
+			}
+		}
+
+		return $next( $request, $response );
 	}
 }
